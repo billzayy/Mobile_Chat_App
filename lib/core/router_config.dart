4b95902c8214b84f0f0cfa90822bf627268c366a
anhnx <x.anh090102@gmail.com>
2023-05-29 09:42:46 +0700

    desgin login, sign

--- lib/core/router_config.dart
+++ lib/core/router_config.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:software_project_3/src/pesentation/app_start.dart';
 import 'package:software_project_3/src/pesentation/login/login_ctrl.dart';
 import 'package:software_project_3/src/pesentation/login/login_view.dart';
 import 'package:software_project_3/src/pesentation/sign/sign_ctrl.dart';
@@ -6,6 +7,10 @@ import 'package:software_project_3/src/pesentation/sign/sign_view.dart';
 
 class RouterConfigs {
   static final List<GetPage> routes = [
+    GetPage(
+      name: AppStartView.routeName,
+      page: () => const AppStartView(),
+    ),
     GetPage(
       name: LoginView.routeName,
       page: () => const LoginView(),
