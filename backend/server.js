09673a137038e2f5a2808de934cbd44544be9f97
Bill Zay <76415515+billzayy@users.noreply.github.com>
2023-06-03 21:11:07 +0700

    main to xanh (#1)
    * Login Database & API
    
    * Change Docker
    
    * add  list, contact , profile (#6)
    
    * Login DB & API V2 (#7)
    
    * Add auth-user api
    
    * Change api/auth-user to api/login
    
    ---------
    
    Co-authored-by: nguyenxuananh0910 <84241450+nguyenxuananh0910@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const sql = require('./sql')
-// Constants
-const PORT = 3000;
 
 // App
 const app = express();
@@ -9,29 +7,77 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
-    res.json({ name: "Tuan" })
+    res.json({ "Do an": "Software-Project-3" })
 })
 
-app.get('/api', (req, res) => {
+app.get('/api/get-user', (req, res) => {
     sql.conSQL("Select * from Login", (recordset) => {
-        res.send(recordset)
+        try {
+            res.status(200).send({
+                "message": "Success",
+                "data": recordset
+            })
+        }
+        catch(err) { 
+            res.status(400).send({
+                "message": "Failure",
+                "data":err
+            })
+        }
     })
 })
 
-app.post('/api', (req, res) => {
-    var name = req.body.name;
-    sql.conSQL(`INSERT INTO Login Values ("${name}")`, (recordset) => {
-        res.send(recordset)
+app.get('/api/login/', (req, res) => {
+    var phone = req.query.phone;
+    var email = req.query.email;
+    var username = req.query.username;
+    sql.conSQL(`Select * from Login Where Phones = '${phone}' OR Email= '${email}' OR Username = '${username}'`, (recordset) => {
+        try {
+            if (recordset[0].Phones == phone || recordset[0].Email == email || recordset[0].Username == username) {
+                res.status(200).send({
+                    "message": "Success",
+                    "data": "Log-in Successful"
+                });
+            }
+        }
+        catch (err) {
+            res.status(400).send({
+                "message": "Failure",
+                "data": "Your username of password is incorrect. Please try again later !"
+            })
+        }
     })
 })
 
-app.delete('/api', (req, res) => {
+app.post('/api/create-user', (req, res) => {
+    var userName = req.body.userName;
+    var password = req.body.password;
+    var email = req.body.email;
+    var fullName = req.body.fullName;
+    var phone = req.body.phone;
+    var status = "online";
+    sql.conSQL(`INSERT INTO Login (Username, Password, Email, Fullname, Phones, Status) Values ('${userName}','${password}','${email}', '${phone}','${fullName}','${status}')`, (recordset) => {
+        try {
+            res.status(201).send({
+                "message": "Success",
+            })
+        }
+        catch (err) {
+            res.status(400).send({
+                "message": "Failure"
+            })
+         }
+    })
+})
+
+
+app.delete('/api/delete-user', (req, res) => {
     var name = req.body.name;
     sql.conSQL(`DELETE FROM Login Where Name = ${name}`, (recordset) => {
         res.send(recordset)
     })
 })
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(8080, () => {
     // console.log(`Running in port ${process.env.PORT}`);
 });
