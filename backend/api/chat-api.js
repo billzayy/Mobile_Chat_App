d266d06991a79b9b1eb3e28b4cea5e435bc98164
Bill Zay <76415515+billzayy@users.noreply.github.com>
2023-06-16 12:19:18 +0700

    Create new server & api get all chat (#19)
    * New Server
    
    * Change db port
    
    * update user api
    
    * change sql db in nodejs
    
    * change response in update user
    
    * Finish Search API & New Message DB
    
    * Add some status code to search
    
    * Add some status code to search
    
    * Another search

---
+++ backend/api/chat-api.js
@@ -0,0 +1,16 @@
+function chatAPI(app, io, sql) { 
+    io.on('connection', (socket) => {
+        socket.on('send-chat-message', (message) => {
+            console.log(message)
+            socket.broadcast.emit('chat-message')
+        })
+    })
+
+    app.get('/api/chat/get-all', (req, res) => {
+        sql.conSQL('SELECT * FROM Message', (recordset) => {
+            res.send(recordset)
+        })
+    })
+}
+
+module.exports = chatAPI;
