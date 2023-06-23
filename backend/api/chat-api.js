9490a8caeca7d7511bab451bd909e0d9261acd5d
Tuan Phan Le <coderbillzay@gmail.com>
2023-06-23 18:48:33 +0700

    chat-message socketio

--- backend/api/chat-api.js
+++ backend/api/chat-api.js
@@ -2,7 +2,7 @@ function chatAPI(app, io, sql) {
     io.on('connection', (socket) => {
         socket.on('send-chat-message', (message) => {
             console.log(message)
-            socket.broadcast.emit('chat-message')
+            socket.broadcast.emit('chat-message', message)
         })
     })
 
